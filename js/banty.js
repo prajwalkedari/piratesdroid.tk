2b5109dbd72dbeb285d7aded7d304e76e9cdfcc2
25prajwal <94011967+25prajwal@users.noreply.github.com>
2022-07-05 03:48:45 +0530

    Create banty.js

---
+++ js/banty.js
@@ -0,0 +1,14 @@
+
+var configgg = {
+    'apiKey': 'AIzaSyAQ_cLzsi4jRJTAXHP1MdgZFvlnd1xfD2U',
+    'authDomain': 'piratesdroid-8c4ed.firebaseapp.com',
+    'databaseURL': 'https://piratesdroid-8c4ed.firebaseio.com',
+    'projectId': 'piratesdroid-8c4ed',
+    'storageBucket': 'piratesdroid-8c4ed.appspot.com',
+    'messagingSenderId': '782951610606',
+    'appId': '1:782951610606:web:3d4616f53c370e23',
+    'measurementId': 'G-5YHQPCK2F2'
+};
+
+firebase.initializeApp(configgg);
+var database = firebase.database(),
