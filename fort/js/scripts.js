96e2c517eeeb6905a42ee9fa7c21fe083795ca9b
25prajwal <94011967+25prajwal@users.noreply.github.com>
2022-07-07 02:53:30 +0530

    Location Map

--- fort/js/scripts.js
+++ fort/js/scripts.js
@@ -78,7 +78,8 @@ var database = firebase.database(),
         .then(data => {var aaa=data.query.pages;document.getElementById("mm").innerHTML+= Object.values(aaa)[0].extract;});
         document.getElementById("Mbody").innerHTML += "<center><h3>Address</h3></center>";
         document.getElementById("Mbody").innerHTML += "<center><p>"+d2[a]+"</p></center>";
-        
+        document.getElementById("Mbody").innerHTML += `<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=`+d3[a]+`&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>`;
+
        
       }
     //   document.getElementById("Mbody").innerText += data.query[0].extract
